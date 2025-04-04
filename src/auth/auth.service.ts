d40cff78a29483398da8b43829b2639cab73d740
Lucassan4225 <145424551+Lucassan4225@users.noreply.github.com>
2025-04-04 14:47:28 +0700

    auth done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,6 +11,16 @@ export class AuthService {
     ) {}
     async validateUser(email: string, password: string): Promise<any> {
         const user = await this.userService.findOne(email);
-        
+        if (user && (await bcrypt.compare(password, user.password))){
+            const { password, ...result} = user.toObject();
+            return result;
+        }return null;
+    }
+
+    async login(user: any) {
+        const payload = {email: user.email,sub: user.id};
+        return{
+             access_token: this.jwtService.sign(payload)
+        };           
     }
 }
